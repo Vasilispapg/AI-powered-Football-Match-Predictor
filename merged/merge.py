d25c23f721afcabd2b6231256b4e342f3462d34e
Vasileios Savvas Papagrigoriou <vasilispapg@outlook.com>
2023-09-01 17:31:04 +0300

    102 Merge function

--- merged/merge.py
+++ merged/merge.py
@@ -2,16 +2,16 @@ import os
 import csv
 
 
-folder=os.listdir('matches_detailed')
+folder=os.listdir('matches_detailed_processed')
 data=[]
 for file_name in folder:
     if file_name.endswith(".csv"):
-        with open('matches_detailed/'+file_name, 'r',encoding='utf-8') as f:
+        with open('matches_detailed_processed/'+file_name, 'r',encoding='utf-8') as f:
             reader = csv.reader(f)
             for row in reader:
                 data.append(row)
                     
-with open('merged/merged_output_detailed2.csv', 'w',encoding='utf-8',newline='') as f:
+with open('merged/merged_output_detailed.csv', 'w',encoding='utf-8',newline='') as f:
     writer = csv.writer(f)
     for item in data:
         writer.writerow(item)
